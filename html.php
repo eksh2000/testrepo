25c9e870f5dc7ac0f3d4e54505841db8506d0fa4
eksh2000 <121233737+eksh2000@users.noreply.github.com>
2022-12-22 17:38:25 +0530

    Add files via upload

---
+++ html.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>REGISTRATION FORM</title>
+
+<link rel="stylesheet" type="text/css" href="./style.css">
+
+</head>
+
+<body>
+<div class="registration-form">
+<h1>Registration form</h1>
+<form action="process.php" method="post">
+<p>Full Name:</p>
+<input type="text" name="fullname" placeholder="Full Name">
+<p>User Name:</p>
+<input type="text" name="username" placeholder="User Name">
+<p>Email:</p>
+<input type="email" name"email" placeholder="Email">
+<p>Password:</P>
+<input type="password" name="password" placeholder="Password">
+<input type="submit" value="submit">
+</form>
+</div>
+</body>
+</html>
